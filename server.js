66648ff9bf56663bf9990ce7cd4a45f2ac683bf6
jonatasthx <2123jonatascorreia@gmail.com>
2025-10-14 20:52:43 -0300

    correção de bugs e adição de delete

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ let pacientes = [
   { ficha: 3, dados: { nome: 'Thx So Much', status: 'em atendimento' } }
 ];
 
-// get - listar todos
-app.get('/pacientes', (req, res) => {
-  res.json(pacientes);
-});
-
-// por status obs:deu trabalho fazer essa merda
 
 app.get('/pacientes', (req, res) => {
   const { status } = req.query;
@@ -27,13 +21,16 @@ app.get('/pacientes', (req, res) => {
   }
 });
 
+app.get('/pacientes', (req, res) => {
+  res.json(pacientes);
+});
 
-/*get - buscar por ficha*/ 
+
 app.get('/pacientes/:ficha', (req, res) => {
   const ficha = parseInt(req.params.ficha);
   const paciente = pacientes.find(i => i.ficha === ficha);
   if (paciente) {
-    res.json(paciente);
+     return res.json(paciente);
   } else {
     res.status(404).json({ error: 'Paciente não encontrado' });
   }
@@ -42,16 +39,13 @@ app.get('/pacientes/:ficha', (req, res) => {
 /*post - adicionar novo paciente*/
 app.post('/pacientes', (req, res) => {
   const { nome, status } = req.body;
-
   if (!nome || !status) {
-    return res.status(400).json({ error: 'Nome e status são obrigatórios' });
+    res.status(400).json({ error: 'Nome e status são obrigatórios' });
   }
-
   const novoPaciente = {
     ficha: pacientes.length + 1,
     dados: { nome, status }
   };
-
   pacientes.push(novoPaciente);
   res.status(201).json(novoPaciente);
 });
@@ -65,10 +59,9 @@ app.put('/pacientes/:ficha', (req, res) => {
   const paciente = pacientes.find(i => i.ficha === ficha);
 
   if (!paciente) {
-    return res.status(404).json({ error: 'Paciente não encontrado' });
+    res.status(404).json({ error: 'Paciente não encontrado' });
   }
 
-  // Atualiza apenas se o dado for fornecido
   if (nome) paciente.dados.nome = nome;
   if (status) paciente.dados.status = status;
 
@@ -76,6 +69,17 @@ app.put('/pacientes/:ficha', (req, res) => {
 });
 
 
+/*apagar cpf*/
+app.delete('/pacientes/:ficha', (req, res) => {
+  const ficha = parseInt(req.params.ficha);
+  const index = pacientes.findIndex(i => i.ficha === ficha);
+  if ( index !== -1 ){
+    const pacienteApagado = pacientes.splice(index, 1);
+    res.json(pacienteApagado[0]);
+  } else {
+      res.status(404).json({ error: 'Paciente não encontrado' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Servidor em execução: http://localhost:${port}`);
